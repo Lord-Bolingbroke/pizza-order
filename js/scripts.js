3dfbda9aad92424e99ec5c70ca74439f65b23ed0
Noah Schooler <noahschooler1@gmail.com>
2018-11-02 13:35:36 -0700

    add cost prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 function Pizza(toppings, size) {
   this.toppings = toppings,
   this.size = size
+  this.price = 0
 }
+
+Pizza.prototype.cost = function() {
+  if (this.size === 10) {
+    this.price = "$6.00"
+  } else if (this.size === 12) {
+    this.price = "$8.00"
+  }
+}
+
+var testPizza = new Pizza(["pepperoni", "mushrooms"], 12)
