e229162153f22a149336bbeb8d1d169646d4f6ef
Noah Schooler <noahschooler1@gmail.com>
2018-11-02 16:00:37 -0700

    add jQuery to display form results

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+function Order() {
+  this.pizzas = []
+}
+
 function Pizza(toppings, size) {
   this.toppings = toppings,
   this.size = size
@@ -7,9 +11,7 @@ function Pizza(toppings, size) {
 Pizza.prototype.cost = function() {
   for(var i = 0; i < this.toppings.length; i++) {
     this.price += 0.50
-    console.log(this.price);
   }
-
   if (this.size === 10) {
     this.price += 6.00
   } else if (this.size === 12) {
@@ -25,3 +27,17 @@ var smallPizza = new Pizza(["pepperoni", "mushrooms"], 10)
 var mediumPizza = new Pizza(["pepperoni", "mushrooms"], 12)
 var largePizza = new Pizza(["pepperoni", "mushrooms"], 14)
 var extraLargePizza = new Pizza(["pepperoni", "mushrooms"], 16)
+
+$(document).ready(function(){
+  $("#pizza-selection").submit(function(event){
+    event.preventDefault();
+    $("#results").show();
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var toppings = $(this).val();
+      console.log(toppings);
+      $("#toppings-result").append(toppings + "<br>");
+    });
+    var size = $("#size").val();
+    $("#size-result").text(size);
+  });
+});
